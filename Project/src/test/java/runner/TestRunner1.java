b220870a613a3a7b6ec09012befef463d01c1a0c
ishantrivedi28 <ishtri7@gmail.com>
2025-04-11 04:07:44 +0000

    made required changes

--- Project/src/test/java/runner/TestRunner1.java
+++ Project/src/test/java/runner/TestRunner1.java
@@ -1,13 +1,29 @@
 package runner;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+import pages.CategoryPageAction1;
+import pages.ElectricCyclePageAction1;
 import pages.HomePageAction1;
 import utils.Base;
+import utils.Reporter;
 
 public class TestRunner1 extends Base{
+
+    public ExtentReports reports;
+    public ExtentTest test;
+
+    @BeforeClass
+    public void reportConfig(){
+        reports = Reporter.createReporter("Avon Cycle");
+    }
 
     @BeforeMethod
     public void initConfig(){
@@ -16,8 +32,13 @@ public class TestRunner1 extends Base{
 
     @Test
     public void testCase(){
+        test = reports.createTest("Test Case 02");
         HomePageAction1 hpa = new HomePageAction1();
-        hpa.hmpageAction();
+        ElectricCyclePageAction1 ecp = new ElectricCyclePageAction1();
+        CategoryPageAction1 cpa = new CategoryPageAction1();
+        hpa.hmpageAction(test);
+        ecp.electricCycleFiltering(test);
+        cpa.categorySelection(test);
     }
 
     @AfterMethod
@@ -25,5 +46,9 @@ public class TestRunner1 extends Base{
     driver.quit();
     }
 
+    @AfterClass
+    public void flushReport(){
 
+        reports.flush();
+    }
 }
